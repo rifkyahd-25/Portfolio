8a80daa931c94c39995393b3ba03aca76ce70fea
Rifky <s22010240@ousl.lk>
2025-03-03 14:00:32 +0530

    NavBar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Contect } from "./pages/Contect";
+import { Education } from "./pages/Education";
+import { Experience } from "./pages/Experience";
+import { Home } from "./pages/Home";
+import { Project } from "./pages/Project";
+import { NavBar } from "./components/NavBar";
 
 export default function App() {
   return (
-    <div>App</div>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="education" element={<Education />} />
+        <Route path="experience" element={<Experience />} />
+        <Route path="project" element={<Project />} />
+        <Route path="contect" element={<Contect />} />
+      </Routes>
+      </Router>
   )
 }
